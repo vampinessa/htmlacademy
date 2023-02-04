61734e24d6c5a62b0117ec3744a0cf624a289258
Koroleva Irina <korolevai@gmail.com>
2023-02-04 16:54:59 +0100

    fix funcs: getPreviewTemplate to correctly return preview; displayPostPreview with using createDocumentFragment

--- js/kekstagram/app/js/preview.js
+++ js/kekstagram/app/js/preview.js
@@ -3,32 +3,29 @@ import { postsSum } from './mock.js';
 
 const previewTemplate = document.querySelector('#picture').content;
 const prewiewNewTemplate = previewTemplate.querySelector('.picture');
-
-
 const picturesContainer = document.querySelector('.pictures');
 
 const getPreviewTemplate = (post) => {
   const preview = prewiewNewTemplate.cloneNode(true);
-  const previewImg = preview.querySelector('.picture__img');
-  const previewLikes = preview.querySelector('.picture__likes');
-  const previewComments = preview.querySelector('.picture__comments');
+  preview.querySelector('.picture__img').src = post.url;
+  preview.querySelector('.picture__img').alt = post.description;
+  preview.querySelector('.picture__likes').textContent = post.likes;
 
-  previewImg.src = post.url;
-  previewImg.alt = post.description;
   let numCommemts = 0;
-  if ((post.comments !== undefined).length) {
+  if ( post.comments !== undefined ) {
     numCommemts = (post.comments).length;
   }
-  previewComments.textContent = numCommemts;
-  previewLikes.textContent = post.likes;
-  picturesContainer.appendChild(preview);
+  preview.querySelector('.picture__comments').textContent = numCommemts;
+  return preview;
 };
 
 const displayPostPreview = () => {
   const posts = generateMockPosts(postsSum);
-  for (let i = 0; i <= (posts.length); i++) {
-    getPreviewTemplate(posts[i]);
-  }
+  const previewTemplateFragment = document.createDocumentFragment();
+  posts.forEach((post) => {
+    previewTemplateFragment.appendChild(getPreviewTemplate(post));
+  });
+  picturesContainer.appendChild(previewTemplateFragment);
 };
 
 export { displayPostPreview };
